b87221957abffc97decac2fefca469b04d1e3567
suresh <suresh12_dmm@yahoo.com>
2020-11-08 10:40:04 +0000

    finalize test cases

--- support_desk/tickets/tests.py
+++ support_desk/tickets/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.conf import settings
 
+
 # Create your tests here.
 class TicketTestCase(TestCase):
     def test_access_ticket_list(self):
@@ -14,15 +15,18 @@ class TicketTestCase(TestCase):
         response = self.client.get('http://127.0.0.1:8000/view/223')
         self.assertEqual(response.status_code, 200)
 
+    def test_ticket_list_pagination(self):
+        """Pagination should return http ok for success access"""
+        response = self.client.get('http://127.0.0.1:8000/?page=1')
+        print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+    def test_ticket_list_pagination_not_found(self):
+        """Pagination should return http ok when entering invalid page manually"""
+        response = self.client.get('http://127.0.0.1:8000/?page=5ss')
+        self.assertEqual(response.status_code, 200)
+
     def test_access_ticket_not_found(self):
         """The invalid ticket id should return 404 status"""
         response = self.client.get('http://127.0.0.1:8000/view/1')
         self.assertEqual(response.status_code, 404)
-
-    def test_api_not_connected(self):
-        """If the connection with Zendesk not success 500 status should return"""
-        settings.AUTH_USER = "test"
-        settings.AUTH_PASS = "test"
-        response = self.client.get('http://127.0.0.1:8000/')
-        self.assertEqual(response.status_code, 500)
-
